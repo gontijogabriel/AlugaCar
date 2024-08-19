334e9ea68e242b903ac2b6798596340638e7b2a9
Gabriel Gontijo <gontijogabr@gmail.com>
2024-08-19 10:30:08 -0300

    feat: new endpoint similar cars

--- backend/cars/urls.py
+++ backend/cars/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from cars.views import CarsListCreateView, CarsDetailView
+from cars.views import CarsListCreateView, CarsDetailView, SimilarCarsView
 
 urlpatterns = [
     path('cars/', CarsListCreateView.as_view(), name='cars-list-create'),
     path('cars/<int:pk>/', CarsDetailView.as_view(), name='car-detail'),
+    path('cars/similar/<str:type>/', SimilarCarsView.as_view(), name='car-similar'),
 ]
--- backend/cars/views.py
+++ backend/cars/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
 
 from cars.models import Cars
 from cars.serializers import CarsSerializer, CarsDetailSerializer
@@ -30,3 +31,28 @@ class CarsDetailView(generics.RetrieveUpdateDestroyAPIView):
         else:
             self.permission_classes = [permissions.IsAuthenticatedOrReadOnly]
         return super().get_permissions()
+
+
+class SimilarCarsView(generics.ListAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = CarsSerializer
+
+    def get_queryset(self):
+        type = self.kwargs.get('type')
+
+        if type:
+            return Cars.objects.filter(type=type.upper())[:4]
+        else:
+            return Cars.objects.none()
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+
+        if not queryset.exists():
+            return Response(
+                {"detail": "No cars found for this category."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+        
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
