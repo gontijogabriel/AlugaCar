5efc6f23c765670841fd11ca3eaa8590d239e810
Gabriel Gontijo <gontijogabr@gmail.com>
2024-08-21 18:50:46 -0300

    feat: update home page and car detail

--- frontend/src/app/[carId]/page.jsx
+++ frontend/src/app/[carId]/page.jsx
@@ -5,32 +5,40 @@ import 'react-datepicker/dist/react-datepicker.css';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import axios from 'axios';
+import Header from '@/components/Header';
+import Loading from '@/components/Loading';
+
+const useCarDetails = (carId) => {
+    const [carDetails, setCarDetails] = useState(null);
+    const [similarCars, setSimilarCars] = useState([]);
+
+
+    useEffect(() => {
+        const fetchDetails = async () => {
+            try {
+                const response = await axios.get(`http://localhost:8000/api/v1/cars/${carId}/`);
+                setCarDetails(response.data);
+
+                const similarResponse = await axios.get(`http://localhost:8000/api/v1/cars/similar/${carId}/${response.data.type}/`);
+                setSimilarCars(similarResponse.data);
+            } catch (error) {
+                console.error('Error fetching car details or similar cars:', error);
+            }
+        };
+
+        fetchDetails();
+    }, [carId]);
+
+    return { carDetails, similarCars };
+};
 
 const CarDetailsPage = ({ params }) => {
     const { carId } = params;
-    const [carDetails, setCarDetails] = useState(null);
-    const [similarCars, setSimilarCars] = useState([]);
+    const { carDetails, similarCars } = useCarDetails(carId);
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
     const [totalRent, setTotalRent] = useState(0);
-
-    const blockedDates = [];
-
-    const fetchCarDetails = async () => {
-        try {
-            const response = await axios.get(`http://localhost:8000/api/v1/cars/${carId}/`);
-            setCarDetails(response.data);
-
-            const similarResponse = await axios.get(`http://localhost:8000/api/v1/cars/similar/${carId}/${response.data.type}/`);
-            setSimilarCars(similarResponse.data);
-        } catch (error) {
-            console.error('Error fetching car details or similar cars:', error);
-        }
-    };
-
-    useEffect(() => {
-        fetchCarDetails();
-    }, [carId]);
+    const [blockedDates, setBlockedDates] = useState([]);
 
     const isDateBlocked = (date) => {
         return blockedDates.some(blockedDate => date.toDateString() === blockedDate.toDateString());
@@ -74,71 +82,117 @@ const CarDetailsPage = ({ params }) => {
         }
     }, [startDate, endDate, carDetails]);
 
-    if (!carDetails) return <p>Loading...</p>;
+    useEffect(() => {
+        if (carDetails && carDetails.rentals) {
+            const blocked = [];
+
+            carDetails.rentals.forEach(rental => {
+                let currentDate = new Date(rental.start_date);
+                const endDate = new Date(rental.end_date);
+
+                while (currentDate <= endDate) {
+                    blocked.push(new Date(currentDate));
+                    currentDate.setDate(currentDate.getDate() + 1);
+                }
+            });
+
+            setBlockedDates(blocked);
+        }
+    }, [carDetails]);
+
+    if (!carDetails) return (
+        <Loading />
+    );
+    
 
     return (
-        <div className="container mx-auto px-4 lg:px-8 py-6">
-            <div className="flex flex-col lg:flex-row gap-6">
+        <>
+            <Header />
+            <main className="container mx-auto px-4 lg:px-8 py-6">
+                <h1 className="text-4xl font-bold mb-4 text-gray-800">{carDetails.model}</h1>
+                <div className="flex flex-col lg:flex-row gap-6">
+                    <div className="flex-1 lg:w-1/2">
+                        <Carousel showThumbs={false} autoPlay infiniteLoop>
+                            {carDetails.images.map((image, index) => (
+                                <div key={index} className="relative h-80">
+                                    <img src={image.url} alt={`Car Image ${index + 1}`} className="w-full h-full object-cover rounded-lg shadow-lg" />
+                                </div>
+                            ))}
+                        </Carousel>
+                    </div>
 
-                <div className="flex-1 lg:w-1/2">
-                    <Carousel showThumbs={false} autoPlay infiniteLoop>
-                        {carDetails.images.map((image, index) => (
-                            <div key={index} className="relative h-80">
-                                <img src={image.url} alt={`Car Image ${index + 1}`} className="w-full h-full object-cover" />
+                    <div className="flex-1 lg:w-1/2 lg:pl-8">
+                        <div className="grid grid-cols-2 sm:grid-cols-2 gap-4 mb-6 p-4 border border-gray-300 rounded-lg bg-white shadow-md">
+                            <div className="text-lg"><strong>Marca:</strong> {carDetails.brand}</div>
+                            <div className="text-lg"><strong>Tipo:</strong> {carDetails.type}</div>
+                            <div className="text-lg"><strong>Categoria:</strong> {carDetails.category}</div>
+                            <div className="text-lg"><strong>Preço / Dia:</strong> R$ {carDetails.day_price}</div>
+                            <div className="text-lg"><strong>Assentos:</strong> {carDetails.seats}</div>
+                            <div className="text-lg"><strong>Km:</strong> {carDetails.odometer}</div>
+                        </div>
+
+                        <div className="mt-6 flex sm:flex-row flex-col gap-4 w-full">
+                            <div className="flex justify-center">
+                                <DatePicker
+                                    selected={startDate}
+                                    onChange={handleSelect}
+                                    startDate={startDate}
+                                    endDate={endDate}
+                                    selectsRange
+                                    excludeDates={blockedDates}
+                                    minDate={new Date()}
+                                    dateFormat="dd/MM/yyyy"
+                                    className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                    placeholderText="Escolha um intervalo de datas"
+                                    inline
+                                />
                             </div>
-                        ))}
-                    </Carousel>
-                </div>
 
-                <div className="flex-1 lg:w-1/2 lg:pl-8">
-                    <h1 className="text-3xl font-bold mb-4">{carDetails.model}</h1>
-                    <p className="text-lg mb-2"><strong>Marca:</strong> {carDetails.brand}</p>
-                    <p className="text-lg mb-2"><strong>Tipo:</strong> {carDetails.type}</p>
-                    <p className="text-lg mb-2"><strong>Categoria:</strong> {carDetails.category}</p>
-                    <p className="text-lg mb-2"><strong>Descrição:</strong> {carDetails.description}</p>
-                    <p className="text-lg mb-2"><strong>Preço por Dia:</strong> R$ {carDetails.day_price}</p>
-                    <p className="text-lg mb-2"><strong>Assentos:</strong> {carDetails.seats}</p>
-                    <p className="text-lg mb-4"><strong>Quilometragem:</strong> {carDetails.odometer} km</p>
+                            {startDate && endDate ? (
+                                <div className="flex-1 lg:flex-col text-center lg:text-left">
+                                    <div className="mb-4 w-full">
+                                        <p><strong>Data de Início:</strong> {startDate.toLocaleDateString()}</p>
+                                        <p><strong>Data de Fim:</strong> {endDate.toLocaleDateString()}</p>
+                                        <p><strong>Número de Dias:</strong> {Math.ceil((endDate - startDate) / (1000 * 60 * 60 * 24)) + 1}</p>
+                                        <p><strong>Preço Total:</strong> R$ {totalRent.toFixed(2)}</p>
+                                    </div>
+                                    <button
+                                        onClick={() => {
+                                            alert(`Aluguel confirmado! Preço total: R$ ${totalRent.toFixed(2)}`);
+                                        }}
+                                        className="w-full px-6 py-3 bg-indigo-600 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                        disabled={!startDate || !endDate}
+                                    >
+                                        Alugar
+                                    </button>
+                                </div>
+                            ) : (
+                                <h1 className='mb-4 w-full'>Selecione uma data de início e outra de fim</h1>
+                            )}
 
-                    <div className="mt-6">
-                        <label className="block text-sm font-medium text-gray-700 mb-2">Selecione o Período de Aluguel</label>
-                        <DatePicker
-                            selected={startDate}
-                            onChange={handleSelect}
-                            startDate={startDate}
-                            endDate={endDate}
-                            selectsRange
-                            excludeDates={blockedDates}
-                            minDate={new Date()}
-                            dateFormat="dd/MM/yyyy"
-                            className="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                            placeholderText="Escolha um intervalo de datas"
-                            inline
-                        />
-                        <div className="mt-4 text-center">
-                            <p className="text-lg font-semibold">Dias Selecionados: {startDate && endDate ? `${Math.ceil(Math.abs(endDate - startDate) / (1000 * 60 * 60 * 24)) + 1}` : '0'}</p>
-                            <p className="text-lg font-semibold">Valor Total do Aluguel: R$ {totalRent.toFixed(2)}</p>
+
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div className="mt-8">
-                <h2 className="text-2xl font-bold mb-4">Carros Semelhantes</h2>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                    {similarCars.map((car) => (
-                        <a href={`${car.id}`} key={car.id} className="block border border-gray-300 rounded-md overflow-hidden shadow-md transition-transform transform hover:scale-105">
-                            <img src={car.image.url} alt={car.model} className="w-full h-40 object-cover" />
-                            <div className="p-4">
-                                <p className="text-lg font-semibold mb-2">{car.model}</p>
-                                <p className="text-gray-600 mb-1">{car.brand}</p>
-                                <p className="text-gray-600">R$ {car.day_price} / dia</p>
-                            </div>
-                        </a>
-                    ))}
+                <p className="text-lg mb-2 p-4">{carDetails.description}</p>
+                <div className="mt-8">
+                    <h2 className="text-2xl font-bold mb-4 text-gray-800">Carros Semelhantes</h2>
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                        {similarCars.map((car) => (
+                            <a href={`${car.id}`} key={car.id} className="block border border-gray-300 rounded-md overflow-hidden shadow-md transition-transform transform hover:scale-105 bg-white">
+                                <img src={car.image.url} alt={car.model} className="w-full h-40 object-cover" />
+                                <div className="p-4">
+                                    <p className="text-lg font-semibold mb-2">{car.model}</p>
+                                    <p className="text-gray-600 mb-1">{car.brand}</p>
+                                    <p className="text-gray-600">R$ {car.day_price} / dia</p>
+                                </div>
+                            </a>
+                        ))}
+                    </div>
                 </div>
-            </div>
-        </div>
+            </main>
+        </>
     );
 };
 
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,7 @@
 'use client'
 
+import Header from '@/components/Header';
+import Loading from '@/components/Loading';
 import { useEffect, useState } from 'react';
 
 
@@ -8,41 +10,42 @@ export default function Home() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-      const fetchCars = async () => {
-          try {
-              const response = await fetch('http://127.0.0.1:8000/api/v1/cars/');
-              const data = await response.json();
-              setCars(data);
-          } catch (error) {
-              console.error("Erro ao buscar os carros:", error);
-          } finally {
-              setLoading(false);
-          }
-      };
+    const fetchCars = async () => {
+      try {
+        const response = await fetch('http://127.0.0.1:8000/api/v1/cars/');
+        const data = await response.json();
+        setCars(data);
+      } catch (error) {
+        console.error("Erro ao buscar os carros:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-      fetchCars();
+    fetchCars();
   }, []);
 
   if (loading) {
-      return <p>Carregando...</p>;
+    return <Loading />;
   }
 
   return (
-    <main className="">
-      ALUGACAR
-
-      <div className=''>
-        {cars.map((car) => (
-            <div key={car.id}>
-              <img width={300} src={car.image.url} alt={car.model}/>
-              <h3>{car.model}</h3>
-              <p>{car.day_price}</p>
-
-              <a href={car.id}>Alugar</a>
-            </div>
-        ))}
-      </div>
-
-    </main>
+    <>
+      <Header />
+      <main className="container mx-auto px-4 lg:px-8 py-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+          {cars.map((car) => (
+            <a href={`${car.id}`} key={car.id} className="block border border-gray-300 rounded-md overflow-hidden shadow-md transition-transform transform hover:scale-105 bg-white">
+              <img src={car.image.url} alt={car.model} className="w-full h-40 object-cover" />
+              <div className="p-4">
+                <p className="text-lg font-semibold mb-2">{car.model}</p>
+                <p className="text-gray-600 mb-1">{car.brand}</p>
+                <p className="text-gray-600">R$ {car.day_price} / dia</p>
+              </div>
+            </a>
+          ))}
+        </div>
+      </main>
+    </>
   );
 }
