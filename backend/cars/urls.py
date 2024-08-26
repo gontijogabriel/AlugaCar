161ae34ba542f2a69d975d85b007f1cbef02965f
Gabriel Gontijo <gontijogabr@gmail.com>
2024-08-26 11:45:31 -0300

    feat: create CarsFilterView for filtering cars by category and type

--- backend/cars/urls.py
+++ backend/cars/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from cars.views import CarsListView, CarDetailView
+from cars.views import CarsListView, CarDetailView, CarsFilterView
 
 urlpatterns = [
     path('cars/', CarsListView.as_view(), name='cars-list'),
     path('cars/<int:pk>/', CarDetailView.as_view(), name='car-detail'),
+    path('cars/filter/', CarsFilterView.as_view(), name='cars-filter'),
 ]
--- backend/cars/views.py
+++ backend/cars/views.py
@@ -1,5 +1,7 @@
-from rest_framework import generics, permissions, status
+from rest_framework import generics, permissions
 from rest_framework.response import Response
+
+from django.db.models import Count
 
 from cars.models import Cars
 from cars.serializers import CarsSerializer, CarsDetailSerializer
@@ -15,3 +17,31 @@ class CarDetailView(generics.RetrieveAPIView):
     permission_classes = [permissions.AllowAny]
     queryset = Cars.objects.all()
     serializer_class = CarsDetailSerializer
+
+
+class CarsFilterView(generics.ListAPIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, *args, **kwargs):
+        category_counts = Cars.objects.values('category').annotate(count=Count('category'))
+        categories = [
+            {
+                'name': item['category'],
+                'count': item['count']
+            } for item in category_counts
+        ]
+
+        type_counts = Cars.objects.values('type').annotate(count=Count('type'))
+        types = [
+            {
+                'name': item['type'],
+                'count': item['count']
+            } for item in type_counts
+        ]
+
+        data = {
+            'categories': categories,
+            'types': types
+        }
+        
+        return Response(data)
