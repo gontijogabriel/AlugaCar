06fc6b67e7e8052cbd62edfb421f75ad99e4ce21
Gabriel Gontijo <gontijogabr@gmail.com>
2024-08-26 12:07:24 -0300

    feat: update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,16 +1,29 @@
 import axios from "axios";
+import { useSession } from "next-auth/react";
+
 
 const api = axios.create({
     baseURL: 'http://127.0.0.1:8000',
 });
 
-// Cars
+
+const createAuthApi = () => {
+    const { data: session } = useSession();
+    return axios.create({
+        baseURL: 'http://127.0.0.1:8000',
+        headers: {
+            Authorization: `Bearer ${session.user.access || ''}`,
+        },
+    });
+};
+
+
 export const getCars = async () => {
     try {
         const response = await api.get('/api/v1/cars');
         return response.data;
     } catch (error) {
-        console.error('Erro ao buscar cars:', error);
+        console.error('Erro ao buscar carros:', error);
         throw error;
     }
 };
@@ -20,14 +33,14 @@ export const getDetailCar = async (carId) => {
         const response = await api.get(`/api/v1/cars/${carId}`);
         return response.data;
     } catch (error) {
-        console.error('Erro ao buscar cars:', error);
+        console.error('Erro ao buscar detalhes do carro:', error);
         throw error;
     }
 };
 
 export const getCarDataFilters = async () => {
     try {
-        const response = await api.get(`/api/v1/cars/filter`);
+        const response = await api.get('/api/v1/cars/filter');
         return response.data;
     } catch (error) {
         console.error('Erro ao buscar dados do filtro dos carros:', error);
@@ -35,3 +48,14 @@ export const getCarDataFilters = async () => {
     }
 };
 
+
+export const getRentalUser = async () => {
+    try {
+        const authApi = createAuthApi();
+        const response = await authApi.get('/api/v1/rental/auth-user/');
+        return response.data;
+    } catch (error) {
+        console.error('Erro ao buscar rentals do usuário:', error);
+        throw error;
+    }
+};
