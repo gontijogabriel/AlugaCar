ab8630bdfa34eedf0d77b8ea84bd8b18a8b9f37d
Gabriel Gontijo <gontijogabr@gmail.com>
2024-08-27 19:57:18 -0300

    feat: create filters, pagination and upgrade rental card

--- frontend/src/app/(auth-routes)/perfil/page.jsx
+++ frontend/src/app/(auth-routes)/perfil/page.jsx
@@ -3,6 +3,7 @@ import { useSession } from 'next-auth/react';
 import Header from "@/components/Header";
 import Link from "next/link";
 import { useEffect, useState } from 'react';
+import Loading from '@/components/Loading';
 
 const Perfil = () => {
 	const { data: session } = useSession();
@@ -10,7 +11,8 @@ const Perfil = () => {
 	const [statusModal, setStatusModal] = useState(false);
 	const [selectedRental, setSelectedRental] = useState(null);
 	const [selected, setSelected] = useState('Em Andamento');
-	
+	const [loading, setLoading] = useState(true);
+
 	const handleSelect = (label) => {
 		setSelected(label);
 	};
@@ -42,7 +44,9 @@ const Perfil = () => {
 					const data = await response.json();
 					setRentals(data);
 				} catch (error) {
-					console.error("Erro: ", error);
+					console.error("Erro ao buscar os carros:", error);
+				} finally {
+					setLoading(false);
 				}
 			};
 
@@ -50,74 +54,98 @@ const Perfil = () => {
 		}
 	}, [session]);
 
+	if (loading) {
+		return <Loading />;
+	}
+
 	return (
 		<>
 			<Header />
-			<main className="container mx-auto px-0 lg:px-8 py-6">
-				<section className="p-4 sm:p-0">
+			<main className="container mx-auto px-0 lg:px-8">
 
-					<nav aria-label="Breadcrumb" className="flex justify-center my-12">
-						<ol className="flex gap-1 bg-gray-200 rounded-lg p-1.5">
-							{['Em Andamento', 'Histórico', 'Meus Dados'].map((label) => (
-							<li
-								key={label}
-								className={`px-4 py-2 rounded-lg cursor-pointer transition-colors duration-300 ${
-								selected === label ? 'bg-white' : 'bg-gray-200'
-								}`}
-								onClick={() => handleSelect(label)}
-							>
-								<a href="/perfil" className="font-semibold text-gray-700">
-								{label}
-								</a>
-							</li>
-							))}
-						</ol>
-					</nav>
+				<section class="bg-white py-4 antialiased dark:bg-gray-900">
+					<div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
+						<div class="mx-auto max-w-5xl">
 
-					<div className="overflow-x-auto">
-						<table className="min-w-full divide-y divide-gray-200 bg-white text-sm rounded-lg border border-gray-200">
-							<thead className="ltr:text-left rtl:text-right">
-								<tr>
-									<th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">Carro</th>
-									<th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 hidden sm:table-cell">Início</th>
-									<th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 hidden sm:table-cell">Término</th>
-									<th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 hidden sm:table-cell">Valor</th>
-									<th className="whitespace-nowrap px-4 py-2 font-medium text-gray-900 hidden sm:table-cell">Status</th>
-									<th className="px-4 py-2"></th>
-								</tr>
-							</thead>
-							<tbody className="divide-y divide-gray-200">
-								{rentals.map((rental) => (
-									<tr key={rental.id}>
-										<td className="whitespace-nowrap px-4 py-2 font-medium text-gray-900">
-											<Link href={`cars/${rental.car.id}`}>
-												{rental.car.model}
-											</Link>
-										</td>
-										<td className="whitespace-nowrap px-4 py-2 text-gray-700 hidden sm:table-cell">
-											{rental.start_date}
-										</td>
-										<td className="whitespace-nowrap px-4 py-2 text-gray-700 hidden sm:table-cell">
-											{rental.end_date}
-										</td>
-										<td className="whitespace-nowrap px-4 py-2 text-gray-700 hidden sm:table-cell">
-											{rental.value}
-										</td>
-										<td className="whitespace-nowrap px-4 py-2 text-gray-700 hidden sm:table-cell">
-											{rental.status}
-										</td>
-										<td className="whitespace-nowrap px-4 py-2">
-											<button
-												onClick={() => openModal(rental)}
-												className="inline-block rounded bg-indigo-600 px-4 py-2 text-xs font-medium text-white hover:bg-indigo-700"
-											>
-												View
-											</button>
-										</td>
-									</tr>
-								))}
-							</tbody>
-						</table>
+							<h2 class="text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">My Orders</h2>
+
+							<div class="flow-root sm:mt-4 mt-2">
+								<div class="divide-y divide-gray-200 dark:divide-gray-700 flex flex-col gap-2">
+
+									{rentals.map((rental) => (
+										<div class="flex flex-wrap items-center gap-y-4 p-2 bg-neutral-100 rounded-md shadow-m" key={rental.id}>
+											<dl class="w-1/2 sm:w-1/4 lg:w-auto lg:flex-1">
+												<dt class="text-base font-medium text-gray-500 dark:text-gray-400">Carro</dt>
+												<dd class="mt-1.5 text-base font-semibold text-gray-900 dark:text-white">
+													<a href={`cars/detail/${rental.car.id}`} class="hover:underline">{rental.car.model}</a>
+												</dd>
+											</dl>
+
+											<dl class="w-1/2 sm:w-1/4 lg:w-auto lg:flex-1">
+												<dt class="text-base font-medium text-gray-500 dark:text-gray-400">Início</dt>
+												<dd class="mt-1.5 text-base font-semibold text-gray-900 dark:text-white">{rental.start_date}</dd>
+											</dl>
+
+											<dl class="w-1/2 sm:w-1/4 lg:w-auto lg:flex-1">
+												<dt class="text-base font-medium text-gray-500 dark:text-gray-400">Preço</dt>
+												<dd class="mt-1.5 text-base font-semibold text-gray-900 dark:text-white">R$ {rental.value}</dd>
+											</dl>
+
+											<dl class="w-1/2 sm:w-1/4 lg:w-auto lg:flex-1">
+												<dt class="text-base font-medium text-gray-500 dark:text-gray-400">Status</dt>
+												<dd class="me-2 mt-1.5 inline-flex items-center rounded bg-primary-100 px-2.5 py-0.5 text-xs font-medium text-primary-800 dark:bg-primary-900 dark:text-primary-300">
+													<svg class="me-1 h-3 w-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+														<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.5 4h-13m13 16h-13M8 20v-3.333a2 2 0 0 1 .4-1.2L10 12.6a1 1 0 0 0 0-1.2L8.4 8.533a2 2 0 0 1-.4-1.2V4h8v3.333a2 2 0 0 1-.4 1.2L13.957 11.4a1 1 0 0 0 0 1.2l1.643 2.867a2 2 0 0 1 .4 1.2V20H8Z" />
+													</svg>
+													{rental.status}
+												</dd>
+											</dl>
+
+											<div class="w-full grid sm:grid-cols-2 lg:flex lg:w-64 lg:items-center lg:justify-end gap-4">
+												<button type="button" class="w-full rounded-lg border border-red-700 px-3 py-2 text-center text-sm font-medium text-red-700 hover:bg-red-700 hover:text-white focus:outline-none focus:ring-4 focus:ring-red-300 dark:border-red-500 dark:text-red-500 dark:hover:bg-red-600 dark:hover:text-white dark:focus:ring-red-900 lg:w-auto">Cancel order</button>
+												<button type="button" onClick={() => openModal(rental)} class="w-full inline-flex justify-center rounded-lg  border border-gray-200 bg-white px-3 py-2 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-primary-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-100 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700 lg:w-auto">View details</button>
+											</div>
+										</div>
+									))}
+								</div>
+							</div>
+
+							<nav class="mt-6 flex items-center justify-center sm:mt-8" aria-label="Page navigation example">
+								<ul class="flex h-8 items-center -space-x-px text-sm">
+									<li>
+										<a href="#" class="ms-0 flex h-8 items-center justify-center rounded-s-lg border border-e-0 border-gray-300 bg-white px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+											<span class="sr-only">Previous</span>
+											<svg class="h-4 w-4 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+												<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m15 19-7-7 7-7" />
+											</svg>
+										</a>
+									</li>
+									<li>
+										<a href="#" class="flex h-8 items-center justify-center border border-gray-300 bg-white px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
+									</li>
+									<li>
+										<a href="#" class="flex h-8 items-center justify-center border border-gray-300 bg-white px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+									</li>
+									<li>
+										<a href="#" aria-current="page" class="z-10 flex h-8 items-center justify-center border border-primary-300 bg-primary-50 px-3 leading-tight text-primary-600 hover:bg-primary-100 hover:text-primary-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+									</li>
+									<li>
+										<a href="#" class="flex h-8 items-center justify-center border border-gray-300 bg-white px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">...</a>
+									</li>
+									<li>
+										<a href="#" class="flex h-8 items-center justify-center border border-gray-300 bg-white px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">100</a>
+									</li>
+									<li>
+										<a href="#" class="flex h-8 items-center justify-center rounded-e-lg border border-gray-300 bg-white px-3 leading-tight text-gray-500 hover:bg-gray-100 hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
+											<span class="sr-only">Next</span>
+											<svg class="h-4 w-4 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+												<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m9 5 7 7-7 7" />
+											</svg>
+										</a>
+									</li>
+								</ul>
+							</nav>
+						</div>
 					</div>
 				</section>
 
--- frontend/src/app/cars/page.jsx
+++ frontend/src/app/cars/page.jsx
@@ -1,15 +1,24 @@
-'use client'
+'use client';
 
 import Header from '@/components/Header';
 import Loading from '@/components/Loading';
 import { useEffect, useState } from 'react';
 import { getCarDataFilters, getCars } from '@/services/api';
-import Filter from '@/components/Filter';
+
+import { useSearchParams, useRouter } from 'next/navigation';
+import { IoIosArrowForward, IoIosSearch } from 'react-icons/io';
 
 const Cars = () => {
   const [filter, setFilter] = useState({ categories: [], types: [] });
   const [cars, setCars] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const searchParams = useSearchParams();
+  const router = useRouter();
 
   useEffect(() => {
     const fetchFilter = async () => {
@@ -26,9 +35,12 @@ const Cars = () => {
 
   useEffect(() => {
     const fetchCars = async () => {
+      const query = { ...Object.fromEntries(searchParams.entries()), page: currentPage };
       try {
-        const data = await getCars();
-        setCars(data);
+        const data = await getCars(query);
+        setCars(data.results);
+        setTotalResults(data.total_results);
+        setTotalPages(Math.ceil(data.count / data.page_size));
       } catch (error) {
         console.error("Erro ao buscar os carros:", error);
       } finally {
@@ -37,7 +49,36 @@ const Cars = () => {
     };
 
     fetchCars();
-  }, []);
+  }, [searchParams, currentPage]);
+
+  const handleCheckboxChange = (type, value) => {
+    const params = new URLSearchParams(searchParams);
+
+    let selectedFilters = params.get(type) ? params.get(type).split(',') : [];
+    if (selectedFilters.includes(value)) {
+      selectedFilters = selectedFilters.filter((item) => item !== value);
+    } else {
+      selectedFilters.push(value);
+    }
+
+    if (selectedFilters.length) {
+      params.set(type, selectedFilters.join(','));
+    } else {
+      params.delete(type);
+    }
+
+    router.push(`?${params.toString()}`);
+  };
+
+  const handleSearch = () => {
+    const params = new URLSearchParams(searchParams);
+    if (searchTerm) {
+      params.set('search', searchTerm);
+    } else {
+      params.delete('search');
+    }
+    router.push(`?${params.toString()}`);
+  };
 
   if (loading) {
     return <Loading />;
@@ -48,37 +89,175 @@ const Cars = () => {
       <Header />
       <main className="container mx-auto px-4 lg:px-8 py-6">
         <div className="flex flex-col lg:flex-row gap-6">
+          <div className="w-full lg:w-1/4 lg:max-w-56">
+            <div className="block rounded-md overflow-hidden">
+              <div className="space-y-2">
 
-          <Filter categories={filter.categories} types={filter.types} />
-          
-          <div className="w-full lg:w-3/4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
-            {cars.length > 0 ? (
-              cars.map((car) => (
-                <a
-                  href={`cars/${car.id}`}
-                  key={car.id}
-                  className="block border border-gray-300 rounded-md overflow-hidden shadow-md transition-transform transform hover:scale-105 bg-white"
-                >
-                  <img
-                    src={car.image.url}
-                    alt={car.model}
-                    className="w-full h-40 object-cover"
-                  />
-                  <div className="p-4">
-                    <p className="text-lg font-semibold mb-2">{car.model}</p>
-                    <p className="text-gray-600 mb-1">{car.brand}</p>
-                    <p className="text-gray-600">R$ {car.day_price} / dia</p>
+                <details className="overflow-hidden rounded border border-gray-300">
+                  <summary className="flex items-center justify-between gap-2 p-4 text-gray-900 transition">
+                    <input 
+                      type="text" 
+                      className="text-sm font-medium outline-none" 
+                      placeholder="Pesquisar"
+                      value={searchTerm}
+                      onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                    <span className="transition group-open:-rotate-180 cursor-pointer" onClick={handleSearch}>
+                      <IoIosSearch />
+                    </span>
+                  </summary>
+                </details>
+
+                <details className="overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden">
+                  <summary className="flex cursor-pointer items-center justify-between gap-2 bg-white p-4 text-gray-900 transition">
+                    <span className="text-sm font-medium"> Categoria </span>
+                    <span className="transition group-open:-rotate-180">
+                      <IoIosArrowForward />
+                    </span>
+                  </summary>
+
+                  <div className="border-t border-gray-200 bg-white">
+                    <ul className="space-y-1 border-t border-gray-200 p-4">
+                      {filter.categories.map((categ, index) => (
+                        <li key={index}>
+                          <label className="inline-flex items-center gap-2">
+                            <input 
+                              type="checkbox" 
+                              className="size-5 rounded border-gray-300" 
+                              onChange={() => handleCheckboxChange('category', categ.name)}
+                              checked={searchParams.get('category')?.split(',').includes(categ.name) || false}
+                            />
+                            <span className="text-sm font-medium text-gray-700">
+                              {`${categ.name} (${categ.count})`}
+                            </span>
+                          </label>
+                        </li>
+                      ))}
+                    </ul>
                   </div>
-                </a>
-              ))
-            ) : (
-              <p className="text-center col-span-full">Nenhum carro disponível no momento.</p>
-            )}
+                </details>
+
+                <details className="overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden">
+                  <summary className="flex cursor-pointer items-center justify-between gap-2 bg-white p-4 text-gray-900 transition">
+                    <span className="text-sm font-medium"> Carroceria </span>
+                    <span className="transition group-open:-rotate-180">
+                      <IoIosArrowForward />
+                    </span>
+                  </summary>
+
+                  <div className="border-t border-gray-200 bg-white">
+                    <ul className="space-y-1 border-t border-gray-200 p-4">
+                      {filter.types.map((tp, index) => (
+                        <li key={index}>
+                          <label className="inline-flex items-center gap-2">
+                            <input 
+                              type="checkbox" 
+                              className="size-5 rounded border-gray-300" 
+                              onChange={() => handleCheckboxChange('type', tp.name)}
+                              checked={searchParams.get('type')?.split(',').includes(tp.name) || false}
+                            />
+                            <span className="text-sm font-medium text-gray-700">
+                              {`${tp.name} (${tp.count})`}
+                            </span>
+                          </label>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </details>
+
+              </div>
+            </div>
+          </div>
+
+          <div className='flex flex-col'>
+            <div className="w-full lg:w-3/4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
+
+              {cars.length > 0 ? (
+                cars.map((car) => (
+                  <a
+                    href={`cars/detail/${car.id}`}
+                    key={car.id}
+                    className="block border border-gray-300 rounded-md overflow-hidden shadow-md transition-transform transform hover:scale-105 bg-white"
+                  >
+                    <img
+                      src={car.image.url}
+                      alt={car.model}
+                      className="w-full h-40 object-cover"
+                    />
+                    <div className="p-4">
+                      <p className="text-lg font-semibold mb-2">{car.model}</p>
+                      <p className="text-gray-600 mb-1">{car.brand}</p>
+                      <p className="text-gray-600">R$ {car.day_price} / dia</p>
+                    </div>
+                  </a>
+                ))
+              ) : (
+                <p className="text-center col-span-full">Nenhum carro disponível no momento.</p>
+              )}
+            </div>
+            <p>resultados {totalResults}</p>
+            <nav>
+              <ol className="flex justify-center gap-1 text-xs font-medium">
+                <li>
+                  <a
+                    onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
+                    className="inline-flex size-8 items-center justify-center rounded border border-gray-100 hover:bg-gray-200 bg-white text-gray-900 rtl:rotate-180 cursor-pointer"
+                  >
+                    <span className="sr-only">Prev Page</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="size-3"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                </li>
+
+                {Array.from({ length: totalPages }, (_, index) => (
+                  <li key={index + 1}>
+                    <a
+                      onClick={() => setCurrentPage(index + 1)}
+                      className={`block size-8 rounded border ${currentPage === index + 1 ? 'border-blue-600 bg-blue-600 text-white' : 'border-gray-100 hover:bg-gray-200 bg-white text-gray-900'} text-center leading-8 cursor-pointer`}
+                    >
+                      {index + 1}
+                    </a>
+                  </li>
+                ))}
+
+                <li>
+                  <a
+                    onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
+                    className="inline-flex size-8 items-center justify-center rounded border border-gray-100 hover:bg-gray-200 bg-white text-gray-900 rtl:rotate-180 cursor-pointer"
+                  >
+                    <span className="sr-only">Next Page</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="size-3"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </a>
+                </li>
+              </ol>
+            </nav>
           </div>
         </div>
       </main>
     </>
   );
-}
+};
 
 export default Cars;
