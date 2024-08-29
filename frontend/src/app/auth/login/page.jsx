29eaa4c8997f206dca818c4a609047643286a881
Gabriel Gontijo <gontijogabr@gmail.com>
2024-08-28 23:55:11 -0300

    feat: update auth pages

---
+++ frontend/src/app/auth/login/page.jsx
@@ -0,0 +1,82 @@
+'use client';
+
+import { signIn } from 'next-auth/react';
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+
+const Login = () => {
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError('');
+
+    const res = await signIn('credentials', {
+      redirect: false,
+      email: formData.email,
+      password: formData.password,
+    });
+
+    if (res.error) {
+      setError(res.error);
+    } else if (res.ok) {
+      router.replace('/')
+    }
+
+    setLoading(false);
+  };
+
+  return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="w-full max-w-md p-8 bg-white border border-gray-200 rounded-lg shadow-md">
+          <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+          <form onSubmit={onSubmit}>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-gray-700">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                required
+                className="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            <div className="mb-6">
+              <label htmlFor="password" className="block text-gray-700">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                required
+                className="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+            </div>
+            {error && (
+              <div className="mb-4 p-2 text-red-600 border border-red-300 rounded-lg bg-red-50">
+                {error}
+              </div>
+            )}
+            <button
+              type="submit"
+              disabled={loading}
+              className={`w-full px-4 py-2 ${loading ? 'bg-blue-300 cursor-not-allowed' : 'bg-blue-600 hover:bg-blue-700'} text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2 focus:ring-blue-500`}
+            >
+              {loading ? 'Loading...' : 'Submit'}
+            </button>
+          </form>
+        </div>
+      </div>
+  );
+};
+
+export default Login;
